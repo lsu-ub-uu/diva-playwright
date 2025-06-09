ba2b3092c0f7438ff4c304e66b3bfd3ab136341b
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-06-09 11:43:00 +0200

    update CI config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig, devices } from '@playwright/test';
 import 'dotenv/config';
 
-const { TARGET_URL = '' } = process.env;
+const { TARGET_URL = '', CI } = process.env;
 
 export default defineConfig({
   testDir: './test',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  forbidOnly: !!CI,
+  retries: CI ? 2 : 0,
+  workers: CI ? 1 : undefined,
+  reporter: CI
+    ? [
+        ['html', { open: 'never' }],
+        ['junit', { outputFile: 'junit-report.xml' }],
+      ]
+    : [['html']],
   use: {
     baseURL: TARGET_URL,
     trace: 'on-first-retry',
