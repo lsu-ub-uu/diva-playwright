8f9a132965fa1b4ad3defb3696b593f43cf135c5
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-09-01 15:07:17 +0200

    update due to metadata changes

--- test/util/logIn.ts
+++ test/util/logIn.ts
@@ -3,7 +3,7 @@ import { expect, type Page } from '@playwright/test';
 export const logIn = async (
   page: Page,
   userName: string = 'DiVA Admin',
-  expectedUserText = 'Diva Admin',
+  expectedUserText = 'DiVA Admin',
 ) => {
   // Log in
   await page.getByRole('button', { name: 'Logga in' }).click();
