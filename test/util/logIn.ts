db36bb53843f200a7ec2713dd46dbcc90ccc2157
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-01-14 11:43:41 +0100

    Update create output test

--- test/util/logIn.ts
+++ test/util/logIn.ts
@@ -2,13 +2,11 @@ import { expect, type Page } from '@playwright/test';
 
 export const logIn = async (
   page: Page,
-  userName: string = 'DiVA Admin',
-  expectedUserText = 'DiVA Admin',
+  userName: string = 'DiVA SystemAdmin',
+  expectedUserText = 'DiVA SystemAdmin',
 ) => {
   // Log in
-  await page
-    .getByRole('button', { name: 'divaClient_LoginButtonText' })
-    .click();
+  await page.getByRole('button', { name: 'divaClient_LoginText' }).click();
   await page.getByRole('menuitem', { name: userName }).click();
   await expect(
     page.getByRole('button', { name: expectedUserText }),
