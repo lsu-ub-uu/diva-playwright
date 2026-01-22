9c977654f311d298d16fc6f379648b060a20cf44
egiiiiil <sixteenth.bird@gmail.com>
2026-01-22 14:29:10 +0100

    Update search tests

--- test/searchOutput.test.ts
+++ test/searchOutput.test.ts
@@ -13,21 +13,46 @@ test.describe('Search output', () => {
       'title',
     );
 
+    const recordId = getFirstDataAtomicValueWithNameInData(
+      getFirstDataGroupWithNameInData(divaOutput, 'recordInfo'),
+      'id',
+    );
+
     await goToPage(page, '/diva-output');
 
-    await page
-      .getByRole('textbox', { name: 'genericSearchTextVarText' })
-      .fill(recordTitle);
+    await page.getByRole('searchbox').fill(recordTitle);
 
-    await expect(
-      await page.getByRole('textbox', { name: 'genericSearchTextVarText' }),
-    ).toHaveValue(recordTitle);
+    await expect(await page.getByRole('searchbox')).toHaveValue(recordTitle);
     await page
       .getByRole('button', { name: 'divaClient_SearchButtonText', exact: true })
       .click();
-
+    const encodedRecordTitle = encodeURIComponent(recordTitle).replaceAll(
+      '%20',
+      '\\+',
+    );
     await expect(
       await page.getByText(recordTitle, { exact: true }),
     ).toBeVisible();
+    await expect(page).toHaveURL(
+      new RegExp(`.*\\/diva-output\\?q=${encodedRecordTitle}&start=1&rows=10`),
+    );
+
+    await page
+      .getByRole('button', { name: 'divaClient_showFiltersText', exact: true })
+      .click();
+    await page
+      .getByRole('textbox', { name: 'searchRecordIdTextVarText' })
+      .fill(recordId);
+
+    const oldId = 'oldId123';
+    await page
+      .getByRole('textbox', { name: 'oldIdSearchTextVarText' })
+      .fill(oldId);
+
+    await expect(page).toHaveURL(
+      new RegExp(
+        `.*\\/diva-output\\?q=${encodedRecordTitle}&start=1&rows=10&recordIdSearchTerm=${recordId}&oldIdSearchTerm=${oldId}`,
+      ),
+    );
   });
 });
