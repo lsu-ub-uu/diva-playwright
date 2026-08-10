4ad780d90a409ee5a06c88408838742d94dab44a
egiiiiil <sixteenth.bird@gmail.com>
2026-08-10 17:22:52 +0200

    Work on tests

--- test/guest/viewOutput.test.ts
+++ test/guest/viewOutput.test.ts
@@ -1,48 +1,28 @@
 import { test } from '../util/fixtures';
 import { expect } from '@playwright/test';
 import { createUrl } from '../util/createUrl';
+import { validationTypes } from '../testData/validationTypes/validationTypes';
 import {
   getFirstDataAtomicValueWithNameInData,
   getFirstDataGroupWithNameInData,
 } from '../util/coraUtils';
 
 test.describe('View output', () => {
-  test('View report', async ({ page, divaOutput }) => {
-    const recordTitle = getFirstDataAtomicValueWithNameInData(
-      getFirstDataGroupWithNameInData(divaOutput, 'titleInfo'),
-      'title',
-    );
-    const recordId = getFirstDataAtomicValueWithNameInData(
-      getFirstDataGroupWithNameInData(divaOutput, 'recordInfo'),
-      'id',
-    );
+  validationTypes.forEach(({ name, validationType }) => {
+    test(`View ${name}`, async ({ page, divaOutputFactory }) => {
+      const divaOutput = await divaOutputFactory(`${validationType}.xml`);
 
-    await page.goto(createUrl(`/diva-output/${recordId}`));
+      const recordId = getFirstDataAtomicValueWithNameInData(
+        getFirstDataGroupWithNameInData(divaOutput, 'recordInfo'),
+        'id',
+      );
 
-    // Title
-    await expect(page.getByRole('heading', { level: 1 }), {
-      message: 'Title',
-    }).toHaveText(recordTitle);
-  });
+      await page.goto(createUrl(`/diva-output/${recordId}`));
 
-  test('View Documented artistic research project (doctoral thesis)', async ({
-    page,
-    divaOutputFactory,
-  }) => {
-    const divaOutput = await divaOutputFactory(
-      'artistic-work_artistic-thesis.xml',
-    );
-
-    const recordId = getFirstDataAtomicValueWithNameInData(
-      getFirstDataGroupWithNameInData(divaOutput, 'recordInfo'),
-      'id',
-    );
-
-    await page.goto(createUrl(`/diva-output/${recordId}`));
-
-    // Title
-    await expect(page.getByRole('heading', { level: 1 }), {
-      message: 'Title',
-    }).toHaveText('TestTitle: TestSubtitle');
+      // Title
+      await expect(page.getByRole('heading', { level: 1 }), {
+        message: 'Title',
+      }).toHaveText('TestTitle: TestSubtitle');
+    });
   });
 });
